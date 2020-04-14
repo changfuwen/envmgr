ba1f8c45f6b62f57cc2f75f53528f5a32c48a4f2
changfuwen <48460515+changfuwen@users.noreply.github.com>
2020-04-14 14:24:29 +0800

    Add files via upload

---
+++ envmgr/src/main/java/com/cfw/envmgr/EnvmgrApplication.java
@@ -0,0 +1,13 @@
+package com.cfw.envmgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvmgrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnvmgrApplication.class, args);
+    }
+
+}
---
+++ envmgr/src/test/java/com/cfw/envmgr/EnvmgrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cfw.envmgr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EnvmgrApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
